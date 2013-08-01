b9a2de0730d9120b103d73463fe4c3bec5341203
Shahnawaz Akhtar <admin@shahnawazAkhtar.com>
2013-08-01 14:41:20 +0530

    no message

---
+++ refund.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Refund Policy Adsonance.com Internet Advertising Company</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Le styles -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+    <link href="assets/css/bootstrap-responsive.css" rel="stylesheet">
+    <link href="assets/css/style.css" rel="stylesheet">
+
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+		<link rel="shortcut icon" href="assets/ico/favicon.png">
+  </head>
+
+<body>
+
+
+
+   <?php include "include/header.php"; ?>
+
+        <h3 class="text-info" align="center">Refund &amp; Cancellation Policy </h3>
+        <div class="well whiteWell">
+          <h4>Adsonance has every incentive to keep customers happy by providing quality advertisement service. To ensure customer satisfaction. </h4>
+          <h3>A Refund Policy is applicable</h3>
+          <ul>
+            <li>In case customer feel that the advertisement is not as shown on the advertisement preview page or as per his/her expectations, customer must bring it to the notice of our customer service within 24 hours , Adsonance Customer Service Team after looking into your complaint will take an appropriate decision. 
+            </li><li>
+If amount getting debited from customer account and Advertisement not displayed within 72 hours , Adsonance will refund the entire charges electronically(as credit to the relevant credit /debit card account used for the transaction), but the bank/card transaction charges are likely to be forfeited. 
+            </li><li>
+If the customer provides a valid reason for requesting a return within 24 hours of purchase advertisement space ,No refunds are permitted after 24 hours of payments . 
+            </li><li>
+If the customer provides a valid report that the charge was fraudulent or unauthorized 
+            </li><li>
+In order to comply with credit card industry rules, ACH industry rules, PayPal terms of service, indian law, and requests from verified Indian judiciary or law enforcement agents, or, For any other reason Adsonance deems appropriate. 
+</li>
+          </ul>
+
+          <h3>Note to Adsonance clients</h3>
+          <ul>
+<li>
+Refund is provided, upon approval, if cancellation request is received 24 hours after customer account is charged. 
+            </li><li>
+This Return and Cancellation Policy is part of, and incorporated within, the Adsonance Client Contract. As a condition of registering with Adsonance and using the Adsonance Services, you expressly acknowledge that you have read and understood this Return and Cancellation Policy and you agree to be bound by the terms and conditions contained within. If at any time you disagree with this Return and Cancellation Policy or any part of it, your sole remedy is to cease all use of the Adsonance Services and terminate your account. Please note, however, that any transactions which occurred prior to the date of such termination shall be governed and controlled in full by the terms of this Return and Cancellation Policy. 
+
+</li>
+
+          </ul>
+
+          <h3>Abuse of the return policy</h3>
+          <p>Customers requesting serial or repeated returns may be blocked from making further advertisement purchases. Customers that violate the terms of use of the service may have their right of return revoked. </p>
+        </div>
+
+<?php include "include/footer.php"; ?>
+
+      </div>
+    </div>
+
+
+
+<script src="assets/js/jquery.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+</body>
+</html>
