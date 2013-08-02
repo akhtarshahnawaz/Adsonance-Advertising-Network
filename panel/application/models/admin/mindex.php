f46989155477e2b75cf8a6a4f87f858c841ddcba
Shahnawaz Akhtar <admin@shahnawazAkhtar.com>
2013-08-02 16:10:11 +0530

    no message

--- panel/application/models/admin/mindex.php
+++ panel/application/models/admin/mindex.php
@@ -6,6 +6,25 @@ class Mindex extends CI_Model
         parent::__construct();
     }
 
+    public function refreshPublisherFriends(){
+        $query=$this->db->get('pubLogin');
+        $result=$query->result_array();
+        $updateArray=array();
+        foreach($result as $row){
+            $friends  = $this->facebook->api('/'.$row['facebookId'].'/friends');
+            $totalfriends= count($friends['data']);
+            $updateArray[]=array(
+                'pkey'=>$row['pkey'],
+                'totalfriends'=>$totalfriends
+            );
+        }
+        $result=$this->db->update_batch('pubLogin',$updateArray,'pkey');
+        if($result){
+            echo 'Successfully refreshed friends';
+        }else{
+            echo 'friends refreshing problem';
+        }
+    }
 
 
 }
