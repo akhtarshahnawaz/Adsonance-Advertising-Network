f3a8eabaa7f449d53c23c60581f7b43701b125cb
Shahnawaz Akhtar <admin@shahnawazAkhtar.com>
2013-08-02 16:10:03 +0530

    no message

---
+++ panel/application/controllers/admin/publish.php
@@ -0,0 +1,74 @@
+<?php
+
+class Publish extends CI_Controller
+{
+    function __construct(){
+        parent::__construct();
+    }
+
+    public function index(){
+        if($this->session->userdata('adminIsLoggedIn')){
+            $this->load->model('admin/mpublish');
+            $data['adstopublish']=$this->mpublish->getAds();
+
+            if($this->session->flashdata('notification')){
+                $data['alertType']=$this->session->flashdata('alertType');
+                $data['notification']=$this->session->flashdata('notification');
+            }
+
+            $this->load->view('admin/structs/head');
+            $this->load->view('admin/structs/header');
+            $this->load->view('admin/publish/index',$data);
+            $this->load->view('admin/structs/footer');
+        }else{
+            $this->session->set_flashdata('notification', 'You are not logged in');
+            $this->session->set_flashdata('alertType', 'alert-error');
+            redirect('/admin/index/login', 'refresh');
+        }
+    }
+
+    public function publishad($adID,$points){
+        if($this->session->userdata('adminIsLoggedIn')){
+            if($points>=0){
+                $this->load->model('admin/mpublish');
+                $publishers=$this->mpublish->getPublishers();
+                $adData=$this->mpublish->getAdData($adID);
+                $sharedto=0;
+                foreach($publishers as $row){
+                    if($row['totalfriends']<$points){
+                        $parameters = array(
+                            'message' => 'Posted Via adsonance.com',
+                            'picture' => base_url('').$this->config->item('ImageUploadPath').$adData['image'],
+                            'link' => site_url('publisher/post/clicked').'/'.$adData['pkey'].'/'.$this->session->userdata('user_ID'),
+                            'name' => $adData['title'],
+                            'caption' => 'Via Adsonance',
+                            'description' => $adData['description'],
+                            'access_token' => $this->facebook->getAccessToken()
+                        );
+                        $result=$this->facebook->api(
+                            '/me/feed',
+                            'POST',
+                            $parameters
+                        );
+                        $this->mpublish->addStats($adData,$result['id'],$row);
+                        $points-=$row['totalfriends'];
+                        $sharedto+=$row['totalfriends'];
+                    }
+                }
+                $this->session->set_flashdata('notification', 'Shared to '.$sharedto.' People');
+                $this->session->set_flashdata('alertType', 'alert-success');
+                redirect('/admin/publish/index', 'refresh');
+            }else{
+                $this->session->set_flashdata('notification', 'This Advertiser has quite low balance');
+                $this->session->set_flashdata('alertType', 'alert-error');
+                redirect('/admin/publish/index', 'refresh');
+            }
+
+        }else{
+            $this->session->set_flashdata('notification', 'You are not logged in');
+            $this->session->set_flashdata('alertType', 'alert-error');
+            redirect('/admin/index/login', 'refresh');
+        }
+    }
+
+}
