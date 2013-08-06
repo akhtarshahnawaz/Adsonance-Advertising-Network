2c1a31a927868d7678ba3335c646f2f11a976b3d
Shahnawaz Akhtar <admin@shahnawazAkhtar.com>
2013-08-06 22:32:34 +0530

    no message

--- panel/application/controllers/admin/publishers.php
+++ panel/application/controllers/admin/publishers.php
@@ -25,4 +25,41 @@ class Publishers extends CI_Controller
         }
     }
 
+
+    public function notify(){
+        if($this->session->userdata('adminIsLoggedIn')){
+            $data=$this->input->post();
+            if(!empty($data)){
+                $notifyTo=$data['publisherKey'];
+                $notifyMsg=$data['inputNotification'];
+
+                $this->load->model('admin/mpublishers');
+                $Publishers=$this->mpublishers->getPublishersFromArray($notifyTo);
+
+                foreach($Publishers as $row){
+                        $parameters=array(
+                            'access_token' => $this->facebook->getAccessToken(),
+                            'href' => 'http://apps.facebook.com/adsonance',
+                            'template' => 'Hi @['.$row['facebookId'].'] '.$notifyMsg
+                        );
+                    try{
+                        $post=$this->facebook->api(
+                        '/'.$row['facebookId'].'/notifications',
+                        'POST',
+                        $parameters
+                        );
+
+                    }catch(FacebookApiException $e){
+                    }
+                }
+                    $this->session->set_flashdata('notification', 'Successfully Notified');
+                    $this->session->set_flashdata('alertType', 'alert-success');
+                    redirect('/admin/publishers/index','refresh');
+            }
+        }else{
+            $this->session->set_flashdata('notification', 'You are not logged in');
+            $this->session->set_flashdata('alertType', 'alert-error');
+            redirect('/admin/index/login', 'refresh');
+        }
+    }
 }
