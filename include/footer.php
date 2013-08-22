0216a0d8576bbbea6e69b7af2137e38fc4a42029
Shahnawaz Akhtar <admin@shahnawazAkhtar.com>
2013-08-22 10:51:53 +0530

    no message

--- include/footer.php
+++ include/footer.php
@@ -2,5 +2,8 @@
       <p  align="center"><a href="career.php">Career</a> | <a href="press-release.php">Press Release</a> | <a href="refund.php">Refund</a> | <a href="privacy.php">Privacy</a> | <a href="terms.php">Terms of use</a>
       </p>
       <p class="copyright" align="center">Copyright © 2013 Adsonance. All Rights Reserved.</p>
-      <p align="center"><span id="siteseal"><script type="text/javascript" src="https://seal.godaddy.com/getSeal?sealID=1obdrgzR1Be77qEvd1RQoXxA8SHMKbyv3VWMfQJ6XqhScyMYQRw"></script></span></p>
+    
+      <p style="margin: 0; padding: 0;" align="center"><img src="assets/img/spns.png" /></p>
+        <p align="center"><span id="siteseal"><script type="text/javascript" src="https://seal.godaddy.com/getSeal?sealID=1obdrgzR1Be77qEvd1RQoXxA8SHMKbyv3VWMfQJ6XqhScyMYQRw"></script></span></p>
+
     </div>
