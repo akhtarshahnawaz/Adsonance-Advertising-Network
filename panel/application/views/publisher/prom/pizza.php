c9ace50be1a3fbe667b1cf9dab8aa1add59aaa43
Shahnawaz Akhtar <admin@shahnawazAkhtar.com>
2013-08-22 10:52:42 +0530

    no message

---
+++ panel/application/views/publisher/prom/pizza.php
@@ -0,0 +1,21 @@
+<div class="container container-fluid" xmlns="http://www.w3.org/1999/html">
+
+    <div class="row well">
+    <div class="span7 pull-left">
+        <img class="span12" style="border-radius: 3px 3px 3px 3px;" src="https://www.adsonance.com/temp/dominospizza.jpg" />
+    </div>
+    <div class="span4 offset 1 pull-right">
+        <div>
+            <p class="lead text-success"  align="center">BT4C6XXX1</p>
+            <p align="center">Get a Free Pizza at Dominos</p>
+            <ul>
+                <li><a href="https://pizzaonline.dominos.co.in/index.php?src=OMG" target="_Blank">Click here </a>to visit Online Site of Dominos India</li>
+                <li>Now Add any two pizza of your choice into the cart</li>
+                <li>At the checkout, apply coupon code  BT4C6XXX1 [Replace X by A,B,C,D,E,F]</li>
+                <li>Make the remaining payment</li>
+            </ul>
+        </div>
+    </div>
+    <div style="clear: both;"></div>
+    </div>
+</div>
