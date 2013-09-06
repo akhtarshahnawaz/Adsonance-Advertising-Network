e97b88f3efd78824fd34f0e5da0b7894330cbf46
Shahnawaz Akhtar <admin@shahnawazAkhtar.com>
2013-09-07 02:44:40 +0530

    no message

--- panel/application/views/admin/publish/index.php
+++ panel/application/views/admin/publish/index.php
@@ -18,7 +18,25 @@
             <th></th>
             </thead>
             <tbody>
-            <?php if($adstopublish):foreach($adstopublish as $row): ?>
+            <?php if($adstopublish['dailyAds']):foreach($adstopublish['dailyAds'] as $row): ?>
+            <tr>
+                <td><img src="<?php echo base_url('').$this->config->item('ImageUploadPath').$row['image']; ?>" width="200px" height="250px" /> </td>
+                <td><?php echo $row['name'];?></td>
+                <td><?php echo $row['title'];?></td>
+                <td><?php echo $row['link'];?></td>
+                <td><?php echo $row['description'];?></td>
+                <td><?php echo $row['points'].' points </br></br> '.$row['remainingBalance'].' USD';?></td>
+
+                <td>
+                    <a class="btn btn-mini btn-success" href="<?php echo site_url('admin/publish/publishad').'/'.$row['pkey'].'/'.$row['points']; ?>"><i class="icon-ok"></i> Publish Ad</a></br></br>
+                </td>
+            </tr>
+                <?php
+            endforeach;
+            endif;
+            ?>
+
+            <?php if($adstopublish['lifetimeAds']):foreach($adstopublish['lifetimeAds'] as $row): ?>
             <tr>
                 <td><img src="<?php echo base_url('').$this->config->item('ImageUploadPath').$row['image']; ?>" width="200px" height="250px" /> </td>
                 <td><?php echo $row['name'];?></td>
