4296a74e242e04408fba517681ebeb8cef012acc
Shahnawaz Akhtar <admin@shahnawazAkhtar.com>
2013-09-06 08:10:30 +0530

    no message

---
+++ panel/application/views/publisher/prom/iphone.php
@@ -0,0 +1,18 @@
+<div class="container container-fluid" xmlns="http://www.w3.org/1999/html">
+
+    <div class="row well">
+    <div class="span7 pull-left">
+        <img class="span12" style="border-radius: 3px 3px 3px 3px;" src="https://www.adsonance.com/temp/iphone.jpg" />
+    </div>
+    <div class="span4 offset 1 pull-right">
+        <div>
+            <p class="lead text-success"  align="center">Get a Free IPhone 5</p>
+            <p align="center">
+                You have succesfully registered for Free IPhone 5 Contest. Winner will be announced on 25th December 2013. You will be notified by E-Mail.
+            </p>
+            
+        </div>
+    </div>
+    <div style="clear: both;"></div>
+    </div>
+</div>
