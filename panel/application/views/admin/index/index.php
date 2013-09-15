be08d1cc71f62395e8e84fe258e5984166b8b217
Shahnawaz Akhtar <admin@shahnawazAkhtar.com>
2013-09-16 02:28:25 +0530

    no message

--- panel/application/views/admin/index/index.php
+++ panel/application/views/admin/index/index.php
@@ -2,7 +2,8 @@
 
     <div class="span6">
 
-        <p class="lead">Withdrawal Requests</p>
+      <?php if(!empty($withdrawalRequests)):?>
+        <p class="lead" align="center">Withdrawal Requests</p>
         <table class="table table-bordered table-condensed table-stripped">
             <thead>
             <td>Amount</td>
@@ -11,17 +12,17 @@
             </thead>
             <tbody>
             <tr>
-                <?php if(!empty($withdrawalRequests)):?>
+          
                 <?php foreach($withdrawalRequests as $row):?>
                 <td><?php $row['amount'].' '.$row['currency']; ?></td>
                 <td><?php $row['method']; ?></td>
                 <td> <a class="btn btn-small btn-primary" href="<?php echo site_url('admin/publishers/withdrawalRequest').'/'.$row['userId'];?>">View Details</a></td>
-                <?php endforeach; else:?>
-                <tr>No Pending Withrawal Requests</tr>
-    <?php endif; ?>
+                <?php endforeach;?>
             </tbody>
             </tr>
         </table>
+            <?php endif; ?>
+
     </div>
     <div class="span5">
 
