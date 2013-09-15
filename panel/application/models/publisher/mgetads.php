9a69ca90186f053219de65e45109551f51223eec
Shahnawaz Akhtar <admin@shahnawazAkhtar.com>
2013-09-16 00:58:22 +0530

    no message

---
+++ panel/application/models/publisher/mgetads.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: sakhtar0092
+ * Date: 15/09/13
+ * Time: 11:19 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class Mgetads
+{
+
+}
